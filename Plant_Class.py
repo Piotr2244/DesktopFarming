565ee12981205a5fe95bc028e69495de6a345d79
Piotr2244 <piotrbenio@wp.pl>
2023-04-23 12:14:49 +0200

    Comments and game optimisation

--- Plant_Class.py
+++ Plant_Class.py
@@ -3,6 +3,8 @@ from PyQt6.QtGui import QPixmap, QIcon
 from PyQt6.QtWidgets import QPushButton, QApplication
 
 
+# class represents a single growing crop, stores info about plant index, growth time, button representing planting
+# spot, plant picture, and more information. After time, plant grows and can be ready to harvest
 class Plant_Class:
     def __init__(self, time: float = 0, place: QPushButton = None, picture: QPixmap = None, index: int = 0,
                  growPicture: QPixmap = None):
@@ -14,12 +16,12 @@ class Plant_Class:
             self.growPicture = growPicture
             self.place.setIcon(QIcon(self.growPicture))
             self.ready = False
-            self.icon_changed = 0
+            self.icon_changed = False
             self.timer = QTimer()
             self.timer.timeout.connect(self.plant_ready)
             QApplication.processEvents()
             self.timer.start(time)
-        else:
+        else:  # no-argument constructor
             self.time = None
             self.place = None
             self.picture = None
@@ -29,9 +31,9 @@ class Plant_Class:
             self.icon_changed = None
             self.timer = None
 
+    # method happens after growth time pass, determines that plant is ready to pick
     def plant_ready(self):
         self.ready = True
-        self.icon_changed += 1
-        if self.icon_changed == 1:
-            self.icon_changed += 1
+        if not self.icon_changed:
+            self.icon_changed = True
             self.place.setIcon(QIcon(self.picture))
